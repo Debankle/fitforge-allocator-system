6fa63b8609356043d474bdf68330883e3e896014
Debankle <oliver.debankle@gmail.com>
2024-06-30 18:59:27 +1000

    CoreService works basically + algorithm implemented

--- src/algorithms/IPL.ts
+++ src/algorithms/IPL.ts
@@ -1,7 +1,61 @@
-// import solver from "javascript-lp-solver";
+import solver from "javascript-lp-solver";
 
-const IPLAllocator = (bvalues: number[][]): number[][] => {
-  return [[1,2,3],[4,5,6]];
+interface Constraints {
+    [key: string]: { max: number };
+}
+
+interface Variables {
+    [key: string]: { score: number } & { [key: string]: number }
+}
+
+interface AllocatorReturn {
+    [key: string]: boolean | number| boolean | number[];
+}
+
+const IPLAllocator = (bvalues: number[][], allocations: number[][], rejections: number[][], allocNums: number[]): AllocatorReturn => {
+    const m = bvalues.length;
+    const n = bvalues[0].length;
+    const constraints: Constraints = {};
+    const variables: Variables = {};
+
+    for (var i = 1; i < m + 1; i++) {
+        constraints[`T${i}`] = { max: 1 };
+    }
+    for (var j = 1; j < n + 1; j++) {
+        constraints[`P${j}`] = { max: allocNums[j-1] }
+    }
+
+    for (var i=1; i < m +1; i++) {
+        for (var j = 1; j < n + 1; j++) {
+            variables[`x_${i}_${j}`] = {
+                score: bvalues[i-1][j-1]
+            };
+            for (var p =1; p < m+1; p++) {
+                variables[`x_${i}_${j}`][`T${p}`] = 0;
+            }
+            for (var q = 1; q < n+1; q++) {
+                variables[`x_${i}_${j}`][`P${q}`] = 0;
+            }
+            variables[`x_${i}_${j}`][`T${i}`] = 1;
+            variables[`x_${i}_${j}`][`P${j}`] = 1;
+        }
+    }
+
+    for (var i = 1; i < allocations.length; i++) {
+        variables[`x_${allocations[i][0]}_${allocations[i][1]}`]['score'] = 9999;
+    }
+    for (var j = 1; j < rejections.length; j++) {
+        variables[`x_${rejections[j][0]}_${rejections[j][1]}`]['score'] = -9999;
+    }
+
+    const model = {
+        optimize: 'score',
+        opType: 'max',
+        constraints: constraints,
+        variables: variables,
+    };
+
+    return solver.Solve(model);
 };
 
 export default IPLAllocator;
