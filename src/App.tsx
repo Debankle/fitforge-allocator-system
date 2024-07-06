32b2b5cd3b37252183ee78f7f51d42804db26752
cingram1186 <129239425+cingram1186@users.noreply.github.com>
2024-07-07 00:03:39 +1000

    Update App.tsx with toolbar
    The toolbar currently does nothing

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,23 @@
 import { useState } from "react";
 import InputComponent from "./components/InputComponent";
+import ToolbarB from "./components/Toolbar_Combine";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Rest from "./pages/Rest";
+import Save from "./pages/Save"
+import Select from "./pages/Select"
+import FitScalar from "./pages/FitScalar"
+import PreferenceScalar from "./pages/PreferenceScalar"
+import Upload from "./pages/Upload"
+import List from "./pages/ListView"
+import SpreadSheet from "./pages/SpreadSheet"
+import Allocations from "./pages/Allocations"
+import Rejections from "./pages/Rejections"
+
 
 function App() {
   // NOTE: Rewrite this to be a function state so data can be passed as needed idk if thats needed
   const [activeComponent, setActiveComponent] = useState("Upload");
+  
 
   const renderComponent = () => {
     /* Pass setActiveComponent to each subcomponent so they can call it when needed
@@ -19,9 +33,9 @@ function App() {
   };
   return (
     <>
-      <h1 className="text-blue-500 text-center">Testing</h1>
-
-      {/* Plans for this section
+      {
+      
+      /* Plans for this section
         Toolbar for testing features
         - reset
         - upload when data is already uploaded
@@ -38,12 +52,34 @@ function App() {
         render the current layout in use
         be able to switch between them
         */}
-
+      
+      {Toolbar()}
       <InputComponent />
-
+      
       {renderComponent()}
     </>
   );
+
+  
+
+
+  function Toolbar() {
+    return <Router>
+      <ToolbarB />
+      <Routes>
+        <Route path='/Rest' element={<Rest />} />
+        <Route path='/Save' element={<Save />} />
+        <Route path='/Select' element={<Select />} />
+        <Route path='/FitScalar' element={<FitScalar />} />
+        <Route path='/PreferenceScalar' element={<PreferenceScalar />} />
+        <Route path='/Upload' element={<Upload />} />
+        <Route path='/List' element={<List />} />
+        <Route path='/SpreadSheet' element={<SpreadSheet />} />
+        <Route path='/Allocations' element={<Allocations />} />
+        <Route path='/Rejections' element={<Rejections />} />
+      </Routes>
+    </Router>;
+  }
 }
 
 export default App;
